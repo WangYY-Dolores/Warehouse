d2aed1c7642862f536c33827f8cb92e923f76888
王媛原 <1299795278@qq.com>
2023-02-27 23:51:32 -0500

    small fix

--- warehouse/FetchData/index.js
+++ warehouse/FetchData/index.js
@@ -7,15 +7,17 @@ module.exports = async function (context, req) {
     const key = '<bYTlxZLrb2d0BQhnn0bb5ZH4zSE8lQZilnf9fR26mG5XUPc0kBNzc4eLn5YbdLWzmoQc15BpgB3YACDbEFF9cQ==>';
     const client = new CosmosClient({ endpoint, key});
     const { database } = await client.databases.createIfNotExists({ id: "warehouseDatabase" });
-    const { container } = await database.containers.createIfNotExists({ id: "container1" });
+    const { container } = await database.containers.createIfNotExists({ id: "container2" });
 
-    if( req.query.shipperId ) {
-        const { resources } = await container.items.query("SELECT * from c WHERE " + req.query.shipperId +  " = c.ShipperId").fetchAll();
+    if( req.query.ShipperID ) {
+        const { resources } = await container.items.query("SELECT * from c WHERE " + req.query.ShipperID +  " = c.ShipperID").fetchAll();
         context.res = {
+            headers: {"Access-Control-Allow-Origin": "*"},
             body: resources
         };
     } else {
         context.res = {
+            headers: {"Access-Control-Allow-Origin": "*"},
             body: []
         };
     }
--- warehouse/PutData/index.js
+++ warehouse/PutData/index.js
@@ -1,39 +1,31 @@
-const { CosmosClient} = require("@azure/cosmos")
+const { CosmosClient } = require("@azure/cosmos")
+const { Buffer } = require('node:buffer')
+
+const fs = require('fs');
 
 module.exports = async function (context, req) {
+    
     context.log('JavaScript HTTP trigger function processed a request.');
 
     const endpoint = "https://warehouse.documents.azure.com:443/";
     const key = '<bYTlxZLrb2d0BQhnn0bb5ZH4zSE8lQZilnf9fR26mG5XUPc0kBNzc4eLn5YbdLWzmoQc15BpgB3YACDbEFF9cQ==>';
     const client = new CosmosClient({ endpoint, key});
     const { database } = await client.databases.createIfNotExists({ id: "warehouseDatabase" });
-    const { container } = await database.containers.createIfNotExists({ id: "container1" });
-    // const { resources } = await container.items.query("SELECT * from c").fetchAll()
-    const name = (req.query.name || (req.body && req.body.name));
-    const shipmentId = (req.query.shipmentId || (req.body && req.body.shipmentId));
+    const { container } = await database.containers.createIfNotExists({ id: "container2" });
+    
+    const content = req.body.toString('utf-8')
+    const str = content.split('\n');
+    str.splice(0, 3)
+    str.splice(-2)
+    const newStr = str.join("\n");
+    const obj = JSON.parse(newStr).Received
 
-    console.log(name)
-    console.log(shipmentId)
-
-    if (shipmentId) {
-        // Edit User
-        await container.items.upsert({
-            shipmentId: shipmentId,
-            name: name, 
-        });
-    } else {
-        // Add User
-        await container.items.create({
-            name: name,
-        });
+    for(let i of obj) {
+        await container.items.upsert(i);
     }
-
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: ""
     };
 }
