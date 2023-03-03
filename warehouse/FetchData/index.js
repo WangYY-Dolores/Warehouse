25dbaab93485f3045a795680c7d78f2aece309b1
王媛原 <1299795278@qq.com>
2023-03-02 22:33:59 -0500

    finish

--- warehouse/FetchData/index.js
+++ warehouse/FetchData/index.js
@@ -1,4 +1,6 @@
 const { CosmosClient} = require("@azure/cosmos")
+// const { KeyClient } = require("@azure/keyvault-keys");
+// const { DefaultAzureCredential } = require("@azure/identity");
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
@@ -9,15 +11,25 @@ module.exports = async function (context, req) {
     const { database } = await client.databases.createIfNotExists({ id: "warehouseDatabase" });
     const { container } = await database.containers.createIfNotExists({ id: "container2" });
 
+    // const keyVaultName = process.env["warehouseKeyVault"];
+    // const url = "https://" + keyVaultName + ".vault.azure.net";
+    // const credential = new DefaultAzureCredential();
+    // const client1 = new KeyClient(url, credential);
+    console.log(req.headers);
+
     if( req.query.ShipperID ) {
         const { resources } = await container.items.query("SELECT * from c WHERE " + req.query.ShipperID +  " = c.ShipperID").fetchAll();
         context.res = {
-            headers: {"Access-Control-Allow-Origin": "*"},
+            headers: {
+                'Access-Control-Allow-Credentials': 'true',
+                'Access-Control-Allow-Methods': 'GET',
+                "Access-Control-Allow-Origin": "http://localhost:3000/"
+            },
             body: resources
         };
     } else {
         context.res = {
-            headers: {"Access-Control-Allow-Origin": "*"},
+            headers: {"Access-Control-Allow-Origin": "http://localhost:3000/"},
             body: []
         };
     }
